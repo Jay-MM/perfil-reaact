ad7574c6301f82b5885a07bcedba3835205812a7
Jesus M <marquez.jay444@gmail.com>
2023-02-21 17:30:30 -0600

    updates to resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,35 +5,48 @@ const Resume = (props) => {
             paddingLeft: '111px'
         }}
         >
-            <div>
-            <h5 className="pb-5">
+            <div className="container-fluid"
+            style={{display: 'flex'}}>
+                <h5 className="pb-5">
                 {/* TODO: finish resume and link here */}
-            Download my Resume <a href="#"><i class=" p-4 bi bi-cloud-download-fill text-white" ></i></a>
-            </h5>
-            <h4 className="pb-4">
-            my skills :</h4>
-            <h4>Front-end :</h4>
-            <ul>
-                <li>PWA</li>
-                <li>React</li>
-                <li>Bootstrap</li>
-                <li>JavaScript</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Design Systems</li>
-    
-            </ul>
-            <h4>Back-end :</h4>
-            <ul>
-                <li>MongoDB, Mongoose</li>
-                <li>Express.js</li>
-                <li>REST</li>
-                <li>Node.js</li>
-                <li>GraphQL</li>
-                <li>MySQL, Sequelize</li>
-                <li>APIs</li>
-    
-            </ul>
+                Download my Resume <a href="#"><i class=" p-4 bi bi-cloud-download-fill text-white" ></i></a>
+                </h5>
+                <div 
+                style={{
+                    display:'flex',
+                    flexDirection:'column',
+                    paddingLeft: '777px'
+                    }}>
+                    <div>
+                        <h1>my skills :</h1>
+                    </div>
+                    <div style={{display:'flex', flexDirection: 'row'}}>
+                        <div>
+                            <h3 className="p-3">Front-end :</h3>
+                            <ul>
+                                <li>PWA</li>
+                                <li>React</li>
+                                <li>Bootstrap</li>
+                                <li>JavaScript</li>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>Design Systems</li>
+                            </ul>
+                        </div>
+                        <div style={{paddingLeft: '111px'}}>
+                            <h3 className="p-3">Back-end :</h3>
+                            <ul>
+                                <li>MongoDB, Mongoose</li>
+                                <li>Express.js</li>
+                                <li>REST</li>
+                                <li>Node.js</li>
+                                <li>GraphQL</li>
+                                <li>MySQL, Sequelize</li>
+                                <li>APIs</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
